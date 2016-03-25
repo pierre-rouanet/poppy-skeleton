4a932e7e3fc688b8053459d3f384a8182d03a757
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-03-25 15:21:02 +0100

    Add skeleton for setup.py file.

---
+++ software/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='poppy-skeleton',
+      version='TO BE COMPLETED',
+      packages=find_packages(),
+
+      install_requires=['pypot >= 2.11', 'poppy-creature >= 1.8'],
+
+      author='TO BE COMPLETED',
+      author_email='TO BE COMPLETED',
+      description='TO BE COMPLETED',
+      url='TO BE COMPLETED',
+      license='TO BE COMPLETED',
+      )
